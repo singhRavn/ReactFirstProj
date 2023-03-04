a6237300ad004accd0c458d63ee228dd5693782c
mac <mac@macs-MacBook-Pro.local>
2023-03-04 11:08:07 +0530

    first commit

---
+++ firstproj/src/App.js
@@ -0,0 +1,7 @@
+function App(){
+    const inputType = 'number';
+    const minValue = 5;
+    return <input style={{border:"1px solid blue"}} textarea autoFocus spellCheck/>
+}
+
+export default App;
